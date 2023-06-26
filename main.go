650220575d1fb8314b421392704b38b37972173f
Danielle Tobler <danielle.tobler210@gmail.com>
2023-06-26 15:26:19 -0500

    Packet struct

--- main.go
+++ main.go
@@ -1,9 +1,38 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net"
 )
 
+type Packet struct {
+	Length    int32
+	RequestID int32
+	Kind      int32
+	Payload   string
+}
+
+func (p Packet) toString(b []byte) string {
+	return string(b[:])
+}
+
+//func (p Packet) generateID() int32 {
+//
+//}
+
 func main() {
-	fmt.Println("Hello world")
+
+	const (
+		HOST = "tobler.games"
+		PORT = "25575"
+		TYPE = "tcp"
+	)
+
+	// Create TCP connection
+	conn, err := net.Dial(TYPE, HOST+":"+PORT)
+	if err != nil {
+		log.Fatalf("Error establishing connection to %s:%s", HOST, PORT)
+	}
+	defer conn.Close()
+
 }
