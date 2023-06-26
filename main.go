c483e5ed03999fbc130bf29b9707d2c4a63ed001
Danielle Tobler <danielle.tobler210@gmail.com>
2023-06-26 15:28:03 -0500

    TCP connection

--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net"
 )
 
 func main() {
-	fmt.Println("Hello world")
+
+	const (
+		HOST = "tobler.games"
+		PORT = "25575"
+		TYPE = "tcp"
+	)
+
+	// Create TCP connection
+	conn, err := net.Dial(TYPE, HOST+":"+PORT)
+	if err != nil {
+		log.Fatalf("Error establishing connection to %s:%s", HOST, PORT)
+	}
+	defer conn.Close()
+
 }
